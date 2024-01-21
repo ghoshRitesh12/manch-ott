74b5cc3170f1d543008e15cbcd896b73b3c03674
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-21 14:56:08 +0530

    feat: remove commented code

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -11,13 +11,3 @@ export const useCounterStore = create<CounterStore>()((set) => ({
   increment: () => set((state) => ({ count: state.count + 1 })),
   decrement: () => set((state) => ({ count: state.count - 1 })),
 }));
-
-// function Counter() {
-//   const { count, inc } = useStore();
-//   return (
-//     <div>
-//       <span>{count}</span>
-//       <button onClick={inc}>one up</button>
-//     </div>
-//   );
-// }
