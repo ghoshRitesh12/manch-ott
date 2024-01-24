fbedd2e79320cf689d41be31e70895e834870369
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-24 19:16:19 +0530

    refactor: made structure more readable

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
 import "@/assets/styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("_manch") as
+  | Element
+  | DocumentFragment;
+
+createRoot(rootElement).render(<App />);
