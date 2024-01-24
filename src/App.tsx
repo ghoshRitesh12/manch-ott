30e1616bb839762705e6ff5e8b63a0680b1209d3
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-24 19:14:47 +0530

    feat: add lazy load page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import HomePage from "@/modules/home/HomePage";
-import LoginPage from "@/modules/login/LoginPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./shared/components/Navbar";
+
+const HomePage = lazy(() => import("@/modules/home/HomePage"));
+const LoginPage = lazy(() => import("@/modules/login/LoginPage"));
+const VideoPage = lazy(() => import("@/modules/video/VideoPage"));
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={<HomePage />}
-          />
-          <Route
-            path="/login"
-            element={<LoginPage />}
-          />
-        </Routes>
-      </Router>
+    <div className="">
+      <BrowserRouter>
+        <Navbar />
+        <Suspense>
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage />}
+            />
+            <Route
+              path="/login"
+              element={<LoginPage />}
+            />
+            <Route
+              path="/video"
+              element={<VideoPage />}
+            />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
