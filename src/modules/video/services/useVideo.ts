d6d69761c6a6a34340bdb3f362228f1af78cbb94
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-25 22:04:16 +0530

    feat: add useVideo service

---
+++ src/modules/video/services/useVideo.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import thumbnailsArray from "@/shared/mockData.json";
+
+function wait(ms: number) {
+  return new Promise<void>((res) => {
+    setTimeout(res, ms);
+  });
+}
+
+async function getVideo(videoId: string) {
+  try {
+    await wait(500);
+    return thumbnailsArray.find((e) => e.id === videoId);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default function useVideo(videoId: string) {
+  return useQuery({
+    queryFn: () => getVideo(videoId),
+    queryKey: ["video_thumbnails", videoId],
+  });
+}
