d7f971a809eda6cdcfbc5044c506cb4ea6c1c15c
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-25 22:03:17 +0530

    feat: implement mock video page

--- src/modules/video/VideoPage.tsx
+++ src/modules/video/VideoPage.tsx
@@ -1,37 +1,78 @@
-import VideoPlayer from "@/shared/components/VideoPlayer";
+// import { Suspense } from "react";
+import useVideo from "./services/useVideo";
+import VideoPlayer from "./components/VideoPlayer";
 import { Button } from "@/shared/components/ui/button";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+import { VideoPathParams } from "./types/pathParams";
+import { Badge } from "@/shared/components/ui/badge";
 
 export default function VideoPage() {
+  const { videoId } = useParams<VideoPathParams>();
+  const { data: videoData, isLoading, isError } = useVideo(videoId as string);
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
+  if (isError) {
+    return <p>an error occured</p>;
+  }
+
   return (
-    <div className="sm:m-2">
+    <div className="sm:m-6">
       <VideoPlayer
-        className="aspect-video max-w-[50rem]"
+        className="aspect-video max-w-[50rem] rounded-xl overflow-hidden"
         option={{
-          url: "https://firebasestorage.googleapis.com/v0/b/manch-a2643.appspot.com/o/Premiums%2F5eb31682-2ff8-43a5-a4b7-b68e1a6c25a2%2Forigin%2FSampleVideo_1280x720_1mb.mp4?alt=media&token=d30c769b-7eb6-425d-8f27-eaafce17f79f",
           container: "#video-player",
+          url: videoData?.videos?.find((u) => u.id === 0)?.src as string,
           thumbnails: {
-            url: "https://firebasestorage.googleapis.com/v0/b/manch-a2643.appspot.com/o/Premiums%2F9db327c2-249a-4e91-b696-349270e8e9b9%2Fthumbnail.jpg?alt=media&token=0f0ccfdf-677a-4afa-8230-0eadf2c852cc",
+            url: videoData?.thumbnail?.src as string,
             column: 4,
             number: 60,
           },
-          // highlight: [
-          //   {
-          //     time: 0.5,
-          //     text: "sample highlight",
-          //   },
-          //   {
-          //     time: 0.58,
-          //     text: "sample highlight",
-          //   },
-          // ],
+          quality: videoData?.videos?.map((q) => {
+            if (q.id === 0) {
+              return {
+                default: true,
+                html: `${q?.resolution}p`,
+                url: q?.src,
+              };
+            }
+            return {
+              html: `${q?.resolution}p`,
+              url: q?.src,
+            };
+          }),
         }}
       />
+
+      <div>
+        <h1 className="block w-fit font-medium text-2xl mt-4">
+          {videoData?.title}
+        </h1>
+
+        <div className="flex gap-x-10 mt-4 align-middle">
+          <Badge
+            variant="secondary"
+            className="text-[1rem]"
+          >
+            {videoData?.category}
+          </Badge>
+
+          <div className="block mt-1">
+            {new Date(
+              videoData?.date?.value?.split("at")?.[0] as string
+            )?.toLocaleDateString()}
+          </div>
+        </div>
+
+        <div className="mt-4">{videoData?.description}</div>
+      </div>
 
       <Button
         variant="default"
         asChild
-        className="mt-4"
+        className="mt-16"
       >
         <NavLink to="/">Go Back</NavLink>
       </Button>
