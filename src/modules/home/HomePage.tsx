90529c1af09f7bf8fa7e1247d1ec9a2afc54f9d7
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-29 23:11:33 +0530

    style: whitespace changes

--- src/modules/home/HomePage.tsx
+++ src/modules/home/HomePage.tsx
@@ -3,9 +3,9 @@ import VideoThumbnailDeck from "./components/VideoThumbnailDeck";
 type HomeProps = {};
 
 export default function Home({}: HomeProps) {
-  return (
-    <div>
-      <VideoThumbnailDeck classes={`py-8 px-8`} />
-    </div>
-  );
+	return (
+		<div>
+			<VideoThumbnailDeck classes={`py-8 px-8`} />
+		</div>
+	);
 }
