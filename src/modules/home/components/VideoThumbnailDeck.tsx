3229a61ff04fea368ea6f6f0a74f2e331efae38f
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-29 23:12:29 +0530

    feat: add suspense & error boundary support

--- src/modules/home/components/VideoThumbnailDeck.tsx
+++ src/modules/home/components/VideoThumbnailDeck.tsx
@@ -1,35 +1,58 @@
 import useVideoThumbnails from "../services/useVideoThumbnails";
 import VideoThumbnailCard from "./VideoThumbnailCard";
 import fc from "@/lib/helpers/classes";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Button } from "@/shared/components/ui/button";
 
 type VideoThumbnailDeckProps = {
-  classes?: string;
+	classes?: string;
 };
 
-export default function VideoThumbnailDeck({
-  classes,
-}: VideoThumbnailDeckProps) {
-  const { data: thumbnails } = useVideoThumbnails();
+function _VideoThumbnailDeck({ classes }: VideoThumbnailDeckProps) {
+	const { data: thumbnails } = useVideoThumbnails();
 
-  return (
-    <div
-      className={fc(`
+	return (
+		<div
+			className={fc(`
         flex justify-start gap-x-8 gap-y-8 flex-wrap
         ${classes}
       `)}
-    >
-      {thumbnails?.map((t) => (
-        <VideoThumbnailCard
-          key={t.id}
-          videoData={{
-            title: t.title,
-            category: t.category,
-            date: t.date.value,
-            src: t.thumbnail.src,
-            id: t.id,
-          }}
-        />
-      ))}
-    </div>
-  );
+		>
+			{thumbnails?.map((t) => (
+				<VideoThumbnailCard
+					key={t.id}
+					videoData={{
+						title: t.title,
+						category: t.category,
+						date: t.date.value,
+						src: t.thumbnail.src,
+						id: t.id,
+					}}
+				/>
+			))}
+		</div>
+	);
+}
+
+export default function VideoThumbnailDeck(props: VideoThumbnailDeckProps) {
+	return (
+		<ErrorBoundary
+			fallbackRender={({ error, resetErrorBoundary }) => (
+				<div>
+					There was an error!{" "}
+					<Button onClick={() => resetErrorBoundary()}>Try again</Button>
+					<pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
+				</div>
+			)}
+		>
+			<Suspense
+				fallback={
+					<h1 className="text-2xl mt-10">THUMBNAIL DECK LOADING....</h1>
+				}
+			>
+				<_VideoThumbnailDeck {...props} />
+			</Suspense>
+		</ErrorBoundary>
+	);
 }
