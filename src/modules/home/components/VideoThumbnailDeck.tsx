1dd0f08ef5980abb0e9e3f84d56f40e421fbdbcd
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-30 20:13:44 +0530

    feat: add better suspense error handling

--- src/modules/home/components/VideoThumbnailDeck.tsx
+++ src/modules/home/components/VideoThumbnailDeck.tsx
@@ -1,8 +1,10 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+
 import useVideoThumbnails from "../services/useVideoThumbnails";
 import VideoThumbnailCard from "./VideoThumbnailCard";
 import fc from "@/lib/helpers/classes";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { Button } from "@/shared/components/ui/button";
 
 type VideoThumbnailDeckProps = {
@@ -37,22 +39,27 @@ function _VideoThumbnailDeck({ classes }: VideoThumbnailDeckProps) {
 
 export default function VideoThumbnailDeck(props: VideoThumbnailDeckProps) {
 	return (
-		<ErrorBoundary
-			fallbackRender={({ error, resetErrorBoundary }) => (
-				<div>
-					There was an error!{" "}
-					<Button onClick={() => resetErrorBoundary()}>Try again</Button>
-					<pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
-				</div>
+		<QueryErrorResetBoundary>
+			{({ reset }) => (
+				<ErrorBoundary
+					onReset={reset}
+					fallbackRender={({ error, resetErrorBoundary }) => (
+						<div>
+							There was an error!{" "}
+							<Button onClick={() => resetErrorBoundary()}>Try again</Button>
+							<pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
+						</div>
+					)}
+				>
+					<Suspense
+						fallback={
+							<h1 className="text-2xl mt-10">THUMBNAIL DECK LOADING....</h1>
+						}
+					>
+						<_VideoThumbnailDeck {...props} />
+					</Suspense>
+				</ErrorBoundary>
 			)}
-		>
-			<Suspense
-				fallback={
-					<h1 className="text-2xl mt-10">THUMBNAIL DECK LOADING....</h1>
-				}
-			>
-				<_VideoThumbnailDeck {...props} />
-			</Suspense>
-		</ErrorBoundary>
+		</QueryErrorResetBoundary>
 	);
 }
